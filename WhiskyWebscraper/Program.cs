7481ba2d6032181c61b10815ae78d176c9eba211
Marcus Bodemann <marcus.bodemann@googlemail.com>
2023-03-18 15:32:49 +0100

    add index page

--- WhiskyWebscraper/Program.cs
+++ WhiskyWebscraper/Program.cs
@@ -9,6 +9,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-//builder.Services.AddSingleton<PeriodicExecutor>();
+builder.Services.AddSingleton<PeriodicExecutor>();
 
 await builder.Build().RunAsync();
